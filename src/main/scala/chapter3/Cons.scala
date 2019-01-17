f15453f87cb4dd004615d61a60812baf23ad6d17
Jeff Hemminger <jeff.hemminger@deciphernow.com>
2019-01-17 11:07:56 -0500

    updating chapter3

--- src/main/scala/chapter3/Cons.scala
+++ src/main/scala/chapter3/Cons.scala
@@ -5,6 +5,31 @@ case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
+
+  def init[A](l: List[A]): List[A] = {
+  }
+  
+  def tail[A](as: List[A]): List[A] = as match {
+    case Nil => as
+    case Cons(x, xs) => xs
+  }
+
+  @annotation.tailrec
+  def drop[A](l: List[A], n: Int): List[A] = n match {
+    case x if x <= 0 => l
+    case _ => drop(tail(l), n - 1)
+  }
+
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(x, xs) if f(x) => dropWhile(xs, f)
+    case _ => l
+  }
+
+  def setHead(v: Int, as: List[Int]): List[Int] = (v, as) match {
+    case (_, Nil) => List(v)
+    case (a,b) => Cons(a, b)
+  }
+
   def sum(ints: List[Int]): Int = ints match {
     case Nil => 0
     case Cons(x, xs) => x + sum(xs)
